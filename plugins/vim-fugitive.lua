78069cb517e9845892bc1db209f272de794add6d
Minuk Choi <6304230+minukchoi@users.noreply.github.com>
2023-05-10 18:19:16 -0400

    add vim-fugitive

---
+++ plugins/vim-fugitive.lua
@@ -0,0 +1,9 @@
+return {
+  "tpope/vim-fugitive",
+  opt = true,
+  cmd = {
+    "G", "Git", "Gdiffsplit", "Gvdiffsplit", "Gedit", "Gsplit",
+    "Gread", "Gwrite", "Ggrep", "Glgrep", "Gmove",
+    "Gdelete", "Gremove", "Gbrowse",
+  },
+}
